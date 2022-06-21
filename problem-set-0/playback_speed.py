4ffa08b62f9960f0a888472755de029b3e17403c
Kevin Perese <kevinp@cbo.gov>
2022-06-21 16:32:37 -0400

    minor edits.

--- problem-set-0/playback_speed.py
+++ problem-set-0/playback_speed.py
@@ -2,8 +2,8 @@
 
 Source: https://cs50.harvard.edu/python/2022/psets/0/indoor/
 
-Some people have a habit of lecturing speaking rather quickly, and it’d be nice to slow them down,
-a la YouTube’s 0.75 playback speed, or even by having them pause between words.
+Some people have a habit of lecturing speaking rather quickly, and it'd be nice to slow them down,
+a la YouTube's 0.75 playback speed, or even by having them pause between words.
 
 In a file called playback.py, implement a program in Python that prompts the user for input and then
 outputs that same input, replacing each space with ... (i.e., three periods).

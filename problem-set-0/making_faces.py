bda184470fa22550bba02476cb126bd2c1548274
Kevin Perese <kevinp@cbo.gov>
2022-06-21 16:33:24 -0400

    minor edits.

--- problem-set-0/making_faces.py
+++ problem-set-0/making_faces.py
@@ -11,7 +11,7 @@ and any :( converted to 🙁 (otherwise known as a slightly frowning face).
 All other text should be returned unchanged.
 
 Then, in that same file, implement a function called main that prompts the user for input, calls
-convert on that input, and prints the result. You’re welcome, but not required, to prompt the user
+convert on that input, and prints the result. You're welcome, but not required, to prompt the user
 explicitly, as by passing a str of your own as an argument to input. Be sure to call main at the
 bottom of your file.
 

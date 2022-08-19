09fd2dbbd476f6714eae8ae4c1a02067ab753761
Kevin Perese <kevinp@cbo.gov>
2022-08-19 11:44:40 -0400

    Minor edits.

--- problem-set-2/vanity_plates.py
+++ problem-set-2/vanity_plates.py
@@ -8,7 +8,7 @@ Among the requirements, though, are:
 * All vanity plates must start with at least two letters.
 * … vanity plates may contain a maximum of 6 characters (letters or numbers) and a minimum of 2 characters.
 * Numbers cannot be used in the middle of a plate; they must come at the end.
-    For example, AAA222 would be an acceptable … vanity plate; AAA22A would not be acceptable.
+    For example, AAA222 would be an acceptable vanity plate; AAA22A would not be acceptable.
     The first number used cannot be a '0'.
 * No periods, spaces, or punctuation marks are allowed.
 
@@ -32,22 +32,22 @@ def main():
 
 
 def is_valid(s):
-    return (starts_with_two_letters(s) and
-        is_right_length(s) and
+    return (is_right_length(s) and
+        starts_with_two_letters(s) and
         ends_with_numeric(s) and
         first_number_not_zero(s) and
         has_no_punctuation(s))
 
 
-def starts_with_two_letters(s):
-    if s[0].isalpha() and s[1].isalpha():
+def is_right_length(s):
+    if 2 <= len(s) <= 6:
         return True
     else:
         return False
 
 
-def is_right_length(s):
-    if 2 <= len(s) <= 6:
+def starts_with_two_letters(s):
+    if s[0].isalpha() and s[1].isalpha():
         return True
     else:
         return False
@@ -77,6 +77,5 @@ def first_number_not_zero(s):
                 return False
             else:
                 return True
-
 
 main()

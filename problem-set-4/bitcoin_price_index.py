d02787587e910eaefcdce4991f5be09d1e05cd6f
Kevin Perese <kevinp@cbo.gov>
2022-11-18 12:01:39 -0500

    minor changes in docstring for bitcoin_price_index.py

--- problem-set-4/bitcoin_price_index.py
+++ problem-set-4/bitcoin_price_index.py
@@ -10,11 +10,13 @@ exchanging one currency (e.g., USD) for Bitcoin.
 
 In a file called bitcoin.py, implement a program that:
 
-Expects the user to specify as a command-line argument the number of Bitcoins, , that they would like to buy.
+Expects the user to specify as a command-line argument the number of Bitcoins, n, that
+they would like to buy.
 
-If that argument cannot be converted to a float, the program should exit via sys.exit with an error message.
-Queries the API for the CoinDesk Bitcoin Price Index at https://api.coindesk.com/v1/bpi/currentprice.json, which returns a JSON object,
-among whose nested keys is the current price of Bitcoin as a float.
+If that argument cannot be converted to a float, the program should exit via sys.exit with an error
+message. Queries the API for the CoinDesk Bitcoin Price Index at
+https://api.coindesk.com/v1/bpi/currentprice.json, which returns a JSON object, among whose nested
+keys is the current price of Bitcoin as a float.
 
 Be sure to catch any exceptions, as with code like:
 import requests

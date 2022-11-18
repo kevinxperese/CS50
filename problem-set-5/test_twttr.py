04c68feab9db1abd02904bd52e497a99445861dd
Kevin Perese <kevinp@cbo.gov>
2022-11-18 12:00:23 -0500

    Adding first problem in problem-set-5.

---
+++ problem-set-5/test_twttr.py
@@ -0,0 +1,10 @@
+import twttr
+
+def test_shorten():
+    assert twttr.shorten('kevin') == 'kvn'
+
+def test_capitalization():
+    assert twttr.shorten('Kurt') == 'Krt'
+
+def test_sentence():
+    assert twttr.shorten('I Love testing in Python!') == '  Lv tstng n Pthn!'

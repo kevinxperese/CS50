eafd3802e8361bdb9a820fdca35f721f3aaf8cd7
Kevin Perese <kevinp@cbo.gov>
2023-02-01 08:57:01 -0500

    Update numb3rs.py and add test_numb3rs.py

--- problem-set-7/numb3rs.py
+++ problem-set-7/numb3rs.py
@@ -1,4 +1,4 @@
-""" Problem Set 7: NUMB3RS
+"""Problem Set 7: NUMB3RS
 Source: https://cs50.harvard.edu/python/2022/psets/7/numb3rs/
 
 An IPv4 address is a numeric identifier that a device (or, on TV, hacker) uses to communicate on
@@ -15,17 +15,87 @@ import re
 
 
 def main():
-    print(validate(input("IPv4 Address: ")))
+    # print(validate_ip(input("IPv4 Address: ")))
+    print(validate_ip('255.30.2.3'))
 
+def validate_ip(ip):
+    """Valiate an IP address based on IPv4 specs of a quad-dotted address with each
+    component between 0 and 255.
 
-def validate(ip):
-    # Source for first capture group: https://www.regextutorial.org/regex-for-numbers-and-ranges.php
-    pattern = re.compile(r'([01]?[0-9][0-9]?|2[0-4][0-9]|25[0-5])(.\1){3}')
+    Parameters
+    ----------
+    ip : str
+        An IP (internet protocol) address
 
-    if pattern.match(ip):
+    Returns
+    -------
+    bool
+        True if valid, false if not
+    """
+
+    # Source for finding numbers in the range 0-255:
+    # https://www.regextutorial.org/regex-for-numbers-and-ranges.php
+    # [01]?[0-9][0-9]?|2[0-4][0-9]|25[0-5]
+    # But this isn't quite right, because it allows "000", which I don't think is a valid IPv4 value.
+    #
+    # The better(?) way is to break up the first or block into even more components:
+    #   1) [0-9] for 0 to 9
+    #   2) [1-9][0-9] for 10 to 99
+    #   3) 1[0-9][0-9] for 100 to 199
+    #   4) 2[0-4][0-9] for 200 to 249
+    #   5) 25[0-5] for 250 to 255
+    valid_ip_num_range = '([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])'
+    pattern = '\.'.join([valid_ip_num_range] * 4)
+
+    if re.match(pattern, ip):
         return True
     else:
         return False
+
+
+def validate_ip_no_re(ip):
+    """Valiate an IP address based on IPv4 specs of a quad-dotted address with each
+    component between 0 and 255.
+
+    This version does not use regular expressions to validate the address.
+
+    Parameters
+    ----------
+    ip : str
+        An IP (internet protocol) address
+
+    Returns
+    -------
+    bool
+        True if valid, false if not
+    """
+
+    # All IP addresses need to have a period in it...
+    if not '.' in ip:
+        return False
+
+    else:
+        nums = ip.split('.')
+
+        # They also need to contain 4 numbers beteween 3 periods...
+        if not len(nums) == 4:
+            return False
+
+        else:
+            for num in nums:
+                # Each num has to be a digit...
+                if not num.isdigit():
+                    return False
+
+                # Each num can't be left padded with zeros...
+                if num.startswith('0') and len(num) > 1:
+                    return False
+
+                # And each num has to be between 0 and 255
+                if not 0 <= int(num) <= 255:
+                    return False
+
+    return True
 
 
 if __name__ == "__main__":
---
+++ problem-set-7/test_numb3rs.py
@@ -0,0 +1,25 @@
+from numb3rs import validate_ip, validate_ip_no_re
+
+
+def test_no_letters():
+    assert validate_ip('a.b.c.d') == False
+    assert validate_ip_no_re('a.b.c.d') == False
+
+def test_correct_number_range():
+    assert validate_ip('1.2.3.4') == True
+    assert validate_ip('1.235.5.7') == True
+    assert validate_ip('1.269.2.3') == False
+    assert validate_ip('0.1.2.3') == True
+    assert validate_ip('-1.-2.-3.-4') == False
+
+    assert validate_ip_no_re('1.2.3.4') == True
+    assert validate_ip_no_re('1.235.5.7') == True
+    assert validate_ip_no_re('1.269.2.3') == False
+    assert validate_ip_no_re('0.1.2.3') == True
+    assert validate_ip_no_re('-1.-2.-3.-4') == False
+
+
+def test_no_left_padding():
+    assert validate_ip('001.000.2.003') == False
+
+    assert validate_ip_no_re('001.000.2.003') == False

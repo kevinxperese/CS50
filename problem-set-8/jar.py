d29210a25a5f34ef541dbc7f66c94ec6c8f4517d
Kevin Perese <kevinp@cbo.gov>
2023-03-10 10:21:22 -0500

    Add jar.py and test_jar.py

--- problem-set-8/jar.py
+++ problem-set-8/jar.py
@@ -5,21 +5,32 @@ Source: https://cs50.harvard.edu/python/2022/psets/8/jar/
 
 class Jar:
     def __init__(self, capacity=12):
-        ...
+        self._capacity = capacity
+        self.num_cookies = 0
+
+        if self._capacity < 0:
+            raise ValueError("Worst cookie jar ever. Try again.")
 
     def __str__(self):
-        ...
+        print("🍪" * self.num_cookies)
 
     def deposit(self, n):
-        ...
+        self.num_cookies += n
+
+        if self.num_cookies > self._capacity:
+            raise ValueError("Oh no! Too many cookies in the cookie jar!")
 
     def withdraw(self, n):
-        ...
+        self.num_cookies -= n
+
+        if self.num_cookies < 0:
+            raise ValueError("You ate all the cookies!")
 
     @property
     def capacity(self):
-        ...
+        return self._capacity
 
     @property
     def size(self):
-        ...
+        return self.num_cookies
+
---
+++ problem-set-8/test_jar.py
@@ -0,0 +1,38 @@
+from jar import Jar
+import pytest
+
+def test_valueerror():
+    with pytest.raises(ValueError):
+        Jar(capacity=-4)
+
+def test_valueerror2():
+    with pytest.raises(ValueError):
+        jar = Jar()
+        jar.withdraw(13)
+
+def test_valueerror3():
+    with pytest.raises(ValueError):
+        jar = Jar()
+        jar.deposit(13)
+
+def test_capacity():
+    jar = Jar()
+    assert jar.capacity == 12
+
+def test_size():
+    jar = Jar()
+    assert jar.size == 0
+
+def test_deposit():
+    jar = Jar()
+    jar.deposit(1)
+
+    assert jar.size == 1
+
+def test_withdraw():
+    jar = Jar()
+    jar.deposit(1)
+    jar.withdraw(1)
+
+    assert jar.size == 0
+

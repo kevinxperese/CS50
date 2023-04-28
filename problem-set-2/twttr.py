80a15319270377ed0fb68aeb2d0a39cb195c4af2
Kevin Perese <kevinp@cbo.gov>
2023-04-28 12:25:59 -0400

    Minor line wrap change.

--- problem-set-2/twttr.py
+++ problem-set-2/twttr.py
@@ -1,12 +1,13 @@
 """CS50P -- Problem Set 2: Just setting up my twttr
 Source: https://cs50.harvard.edu/python/2022/psets/2/twttr/
 
-When texting or tweeting, it's not uncommon to shorten words to save time or space,
-as by omitting vowels, much like Twitter was originally called twttr.
+When texting or tweeting, it's not uncommon to shorten words to save
+time or space, as by omitting vowels, much like Twitter was originally
+called twttr.
 
-In a file called twttr.py, implement a program that prompts the user for a str of text and then
-outputs that same text but with all vowels (A, E, I, O, and U) omitted, whether inputted in
-uppercase or lowercase.
+In a file called twttr.py, implement a program that prompts the user for
+a str of text and then outputs that same text but with all vowels 
+(A, E, I, O, and U) omitted, whether inputted in uppercase or lowercase.
 """
 
 user_text = input('Input: ')

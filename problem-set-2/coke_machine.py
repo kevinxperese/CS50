92ff8494edf6a3e8ddeec1cf97b1583be007ad28
Kevin Perese <kevinp@cbo.gov>
2023-04-28 12:24:54 -0400

    minor line wrap changes.

--- problem-set-2/coke_machine.py
+++ problem-set-2/coke_machine.py
@@ -1,15 +1,15 @@
 """CS50P -- Problem Set 2: Coke Machine
 Source: https://cs50.harvard.edu/python/2022/psets/2/coke/
 
-Suppose that a machine sells bottles of Coca-Cola (Coke) for 50 cents and only accepts coins in
-these denominations: 25 cents, 10 cents, and 5 cents.
+Suppose that a machine sells bottles of Coca-Cola (Coke) for 50 cents and 
+only accepts coins in these denominations: 25 cents, 10 cents, and 5 cents.
 
-In a file called coke.py, implement a program that prompts the user to insert a coin, one at a time,
-each time informing the user of the amount due.
+In a file called coke.py, implement a program that prompts the user to insert
+a coin, one at a time, each time informing the user of the amount due.
 
-Once the user has inputted at least 50 cents, output how many cents in change the user is owed.
-Assume that the user will only input integers, and ignore any integer that isn't an accepted
-denomination.
+Once the user has inputted at least 50 cents, output how many cents in change
+the user is owed. Assume that the user will only input integers, and ignore
+any integer that isn't an accepted denomination.
 """
 
 amount_due = 50
